35edf6155cdebb6e73450bc3de349be6444fdab5
Brad Chase <brad@webappfromscratch.com>
2016-04-23 07:39:56 -0400

    parse more out of //entry/sense e.g. dial, info

--- lib/jmdict/entry/sense.ex
+++ lib/jmdict/entry/sense.ex
@@ -3,29 +3,32 @@ defmodule JMDict.Entry.Sense do
 
   defstruct \
     glosses: [],
-    pos:     []
-  # sense: [ # SENSES (GLOSSES)
-  #   ~x{./sense}le,
-  #   stagk:   ~x{./stagk/text()}ls,
-  #   stagr:   ~x{./stagr/text()}ls,
-  #   xref:    ~x{./xref/text()}ls,    # full ex: <xref>彼・あれ・1</xref>
-  #   pos:     ~x{./pos/text()}ls,     # prior ./sense/pos apply, unless new added
-  #   field:   ~x{./field/text()}ls,
-  #   misc:    ~x{./misc/text()}ls,    # "usually apply to several senses"
-  #   lsource: ~x{./lsource/text()}ls, # attr xml:lang="eng" (default) ISO 639-2
-  #   # attr ls_type="full"(default) || "part"
-  #   # attr ls_wasei="y" means "yes" e.g. waseieigo
-  #   dial:    ~x{./dial/text()}ls,
-  #   gloss:   ~x{./gloss/text()}ls,   # attr xml:lang="eng" (default)
-  #   # attr g_gend "gender of the gloss"
-  #   # pri: ~x{./pri/text()}ls,       # DNE in current JMdict_e file
-  #   s_inf: ~x{./s_inf/text()}ls
-  # ]
+    lsource: [],
+    pos:     [],
+    dial:    [],
+    info:    [],
+    misc:    [],
+    field:   [],
+    xrefs:   [],
+    stagk:   [],
+    stagr:   []
+
 
   def from_element(sense) do
     struct __MODULE__, %{
-      glosses: sense.gloss,
-      pos:     XMLEntities.vals_to_names sense.pos
+      glosses: sense.gloss, # attr g_gend "gender of the gloss"
+  #   lsource: # attr xml:lang="eng" (default) ISO 639-2
+  #     # attr ls_type="full"(default) || "part"
+  #     # attr ls_wasei="y" means "yes" e.g. waseieigo
+
+      pos:     XMLEntities.vals_to_names(sense.pos),
+      dial:    XMLEntities.vals_to_names(sense.dial),
+      misc:    XMLEntities.vals_to_names(sense.misc),
+      field:   XMLEntities.vals_to_names(sense.field),
+      info:    sense.s_inf,
+      xrefs:   sense.xref,
+      stagk:   sense.stagk,
+      stagr:   sense.stagr,
     }
   end
 end
--- test/jmdict_test.exs
+++ test/jmdict_test.exs
@@ -4,9 +4,7 @@ defmodule JMDictTest do
   alias JMDict.XMLEntities
 
   setup do
-    entries = JMDict.entries_stream
-
-    {:ok, entries: entries}
+    {:ok, entries: JMDict.entries_stream}
   end
 
   test "ets xml entity val->key lookup" do
@@ -18,7 +16,12 @@ defmodule JMDictTest do
     assert XMLEntities.val_to_name_map["abbreviation"] == "abbr"
   end
 
-  test "provides kanji + info", %{entries: entries} do
+  test "parses eid", %{entries: entries} do
+    akarasama = get_entry_by_eid entries, 1000225
+    assert akarasama.eid == "1000225"
+  end
+
+  test "parses kanji + info", %{entries: entries} do
     akarasama = get_entry_by_eid entries, 1000225
     assert match? [%{text: "明白", info: ["ateji"]}|_], akarasama.kanji
   end
@@ -28,12 +31,29 @@ defmodule JMDictTest do
     assert match? [_,_,_,%{text: "あしこ", info: ["ok"]}|_], asoko.kana
   end
 
-  test "parses sense glosses + pos", %{entries: entries} do
+  test "parses sense glosses, pos, etc", %{entries: entries} do
+    # eids = [1000920, 1001390, 1002480, 1000320]
+    # [irasshai, oden, otenba, asoko] = get_entries_by_eids entries, eids
     irasshai = get_entry_by_eid entries, 1000920
-    assert match? "1000920", irasshai.eid
-    assert match? 0, length(irasshai.kanji)
-    assert match? [_, %{text: "いらしゃい", info: ["ik"]}], irasshai.kana
+    oden     = get_entry_by_eid entries, 1001390
+    otenba   = get_entry_by_eid entries, 1002480
+    asoko    = get_entry_by_eid entries, 1000320
+    hanpen   = get_entry_by_eid entries, 1010230
+
+    # GLOSS & POS
     assert match? [%{glosses: ["come"|_], pos: [_,"n"]},_], irasshai.senses
+    # MISC & INFO
+    info = "used as a polite imperative"
+    assert match? [%{misc: ["hon"], info: [^info]},_], irasshai.senses
+    # XREFS
+    assert match? [_,%{xrefs: ["いらっしゃいませ"]}], irasshai.senses
+    # FIELD
+    assert match? [%{field: ["food"]}], oden.senses
+    # STAGK
+    assert match? [_,%{stagk: ["半片"]}], hanpen.senses
+    # STAGR
+    assert match? [_,%{stagr: ["あそこ",_]},_], asoko.senses
+    # TODO LSOURCE # assert # <lsource xml:lang="dut">ontembaar</lsource>
   end
 
   def get_entry_by_eid(entries, eid) do
@@ -44,40 +64,47 @@ defmodule JMDictTest do
     |> Enum.to_list
     |> List.last
   end
+
+  def get_entries_by_eids(entries, eids) do
+    entries
+    |> Stream.take_while(& String.to_integer(&1.eid) <= Enum.max(eids))
+    |> Stream.reject(fn entry -> Enum.any?(eids, & entry.eid != &1) end)
+    |> Enum.to_list
+  end
 end
 
-    # %JMDict.Entry{
-    #   eid: "1000920",
-    #
-    #   kanji: [],
-    #
-    #   kana: [
-    #     [
-    #       %Reading{
-    #         text:    "いらっしゃい",
-    #         info:     [],
-    #         priority: ["spec1"],
-    #       },
-    #       %Reading{
-    #         text:    "いらしゃい",
-    #         info:     ["ik"],
-    #         priority: [],
-    #       }
-    #   ],
-    #
-    #   senses: [
-    #     %Sense{
-    #       glosses: ["welcome!"],
-    #       pos:     ["int", "n"],
-    #       # misc:    ["hon"],
-    #       # field:   ["hon"],
-    #       # dial:    ["hon"],
-    #       xrefs:   ["いらっしゃる・1"],
-    #       info:    ["used as a polite imperative"]
-    #     },
-    #     %Sense{
-    #       glosses: ["come", "go", "stay"],
-    #       xrefs:   ["いらっしゃいませ"]
-    #     }
-    #   ]
-    # }
+# %JMDict.Entry{
+#   eid: "1000920",
+#
+#   kanji: [],
+#
+#   kana: [
+#     [
+#       %Reading{
+#         text:    "いらっしゃい",
+#         info:     [],
+#         priority: ["spec1"],
+#       },
+#       %Reading{
+#         text:    "いらしゃい",
+#         info:     ["ik"],
+#         priority: [],
+#       }
+#   ],
+#
+#   senses: [
+#     %Sense{
+#       glosses: ["welcome!"],
+#       pos:     ["int", "n"],
+#       # misc:    ["hon"],
+#       # field:   ["hon"],
+#       # dial:    ["hon"],
+#       xrefs:   ["いらっしゃる・1"],
+#       info:    ["used as a polite imperative"]
+#     },
+#     %Sense{
+#       glosses: ["come", "go", "stay"],
+#       xrefs:   ["いらっしゃいませ"]
+#     }
+#   ]
+# }
